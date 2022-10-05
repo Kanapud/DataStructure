39ae150607c6dc417ce1f6f037b84047ae0d1bac
KANAPAT PANAIEM <6452300555@stu.pim.ac.th>
2022-10-05 11:43:26 +0700

    update

---
+++ Stacks/Stack.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Stacks
+{
+    public interface Stack
+    {
+        bool isempty();
+        int size();
+        void push(object e);
+        object peek();
+        object pop();
+    }
+}
