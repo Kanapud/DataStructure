f29edb3497e6c84ff8dcfa71c8d6da57f5d3a67c
Kanapud <78951238+Kanapud@users.noreply.github.com>
2022-11-01 00:09:33 +0700

    Tree Update

---
+++ Tree/BSTree.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    public class BSTree : BinaryTree
+    {
+        private int SIZE;
+        public object getMax()
+        {
+            Node r = root;
+            if (r == null) return null;
+            while (r.right != null)
+                r = r.right;
+            return r.e;
+        }
+        public object getMin()
+        {
+            Node r = root;
+            if (r == null) return null;
+            while (r.left != null)
+                r = r.left;
+            return r.e;
+        }
+        private Node add(Node r, object e)
+        {
+            if (r == null) { r = new Node(e, null, null); SIZE++; }
+            else
+            {
+                int cmp = ((IComparable)e).CompareTo(r.e);
+                if (cmp < 0)
+                    r.left = add(r.left, e);
+                else
+                    r.right = add(r.right, e);
+            }
+            return r;
+        }
+        public void add(object e) { root = add(root, e); }
+        private Node remove(Node r, object e)
+        {
+            if (r == null) return r;
+            int cmp = ((IComparable)e).CompareTo(r.e);
+            if (cmp < 0) r.left = remove(r.left, e);
+            else if (cmp > 0) r.right = remove(r.right, e);
+            else
+            {
+                if (r.left == null || r.right == null)
+                {
+                    r = r.left == null ? r.right : r.left;
+                    SIZE--;
+                }
+                else
+                {
+                    Node m = r.right;
+                    while (m.left != null)
+                        m = m.left;
+                    r.e = m.e;
+                    r.right = remove(r.right, m.e);
+                }
+            }
+            return r;
+        }
+        public void remove(object e)
+        {
+            root = remove(root, e);
+        }
+        private Node getNode(Node r, object e)
+        {
+            if (r == null) return null;
+            int cmp = ((IComparable)e).CompareTo(r.e);
+            if (cmp == 0) return r;
+            if (cmp < 0) return getNode(r.left, e);
+            else return getNode(r.right, e);
+        }
+        public bool contains(object e)
+        {
+            Node node = getNode(root, e);
+            return node == null ? false : true;
+        }
+        public static void treeSoft(ref object[] x)
+        {
+            BSTree t = new BSTree();
+            for (int i = 0; i < x.Length; i++)
+                t.add(x[i]);
+            x = t.toArray();
+        }
+    }
+}
---
+++ Tree/BinaryTree.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Tree
+{
+    public class BinaryTree
+    {
+        protected Node root;
+        public class Node
+        {
+            public object e;
+            public Node left, right;
+            public Node(object e, Node left, Node right)
+            {
+                this.e = e;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        public int numNodes()
+        {
+            return numNodes(root);
+        }
+        private int numNodes(Node node)
+        {
+            if (node == null) return 0;
+            return 1 + numNodes(node.left) + numNodes(node.right);
+        }
+        public int depth()
+        {
+            return depth(root);
+        }
+        private int depth(Node node)
+        {
+            if (node == null) return -1;
+            return 1 + Math.Max(depth(node.left), depth(node.right));
+        }
+        public object[] toArray()
+        {
+            object[] a = new object[numNodes()];
+            toArray(root, a, 0);
+            return a;
+        }
+        private int toArray(Node node, object[] a, int k)
+        {
+            if (node == null) return k;
+            k = toArray(node.left, a, k);
+            a[k++] = node.e;
+            k = toArray(node.right, a, k);
+            return k;
+        }
+        public void generateTree()
+        {
+            root = new Node('A',
+                new Node('B', new Node('D', null, null), null),
+                new Node('C', null, null)
+                );
+        }
+    }
+}
